643321f469e15d5c94774c586f49b29be3907321
brayevalerien <valerien.brayee@gmail.com>
2025-11-17 03:47:25 +0100

    implement multi-clients using go routines and message broadcasting

--- src/client.go
+++ src/client.go
@@ -20,23 +20,31 @@ func main() {
 
 	log.Println("Connected to server!")
 
-	scanner := bufio.NewScanner(os.Stdin) // scans user input
-	reader := bufio.NewReader(connection) // read data from server
+	go receiveMessages(connection)
 
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		// read user input
 		fmt.Print("You: ")
-		scanner.Scan()
-		message := scanner.Text()
-
-		// send message to the server
-		fmt.Fprint(connection, message+"\n")
-		response, err := reader.ReadString('\n')
-		if err != nil {
-			log.Println("Server disconnected:", err)
+		if !scanner.Scan() {
 			break
 		}
-		fmt.Print("Server: ", response)
+		message := scanner.Text()
+		_, err := fmt.Fprint(connection, message+"\n")
+		if err != nil {
+			log.Println("Failed to send message:", err)
+			os.Exit(1)
+		}
+	}
+}
 
+func receiveMessages(connection net.Conn) {
+	reader := bufio.NewReader(connection)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println("Server disconnected:", err)
+			os.Exit(0)
+		}
+		fmt.Print("Received: ", message)
 	}
 }
--- src/server.go
+++ src/server.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
+	"sync"
 )
+
+var clients []net.Conn      // stores connected clients
+var clientsMutex sync.Mutex // mutex on clients
 
 func main() {
 	log.Println("Starting chat server...")
@@ -20,24 +23,53 @@ func main() {
 
 	log.Println("Server listening on", port)
 
-	connection, err := listener.Accept()
-	if err != nil {
-		log.Fatal("Failed to accept client connection:", err)
-	}
-	defer connection.Close()
-
-	log.Println("Client connected!")
-
-	reader := bufio.NewReader(connection) // read from client
-
 	for {
-		clientMessage, err := reader.ReadString('\n')
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Println("Failed to accept connection:", err)
+			continue
+		}
+		go handleNewClient(connection)
+	}
+}
+
+func handleNewClient(connection net.Conn) {
+	defer connection.Close()
+	clientsMutex.Lock()
+	clients = append(clients, connection)
+	clientsMutex.Unlock()
+	defer removeClient(connection)
+	log.Println("New client connected. Current number of connections:", len(clients))
+	reader := bufio.NewReader(connection)
+	for {
+		message, err := reader.ReadString('\n')
 		if err != nil {
 			log.Println("Client disconnected:", err)
-			break
+			return
 		}
-		log.Println("Client:", clientMessage)
+		log.Println("Client message:", message)
+		broadcast(message, connection)
+	}
+}
 
-		fmt.Fprint(connection, clientMessage)
+// sends a message to all clients other than sender
+func broadcast(message string, sender net.Conn) {
+	clientsMutex.Lock()
+	defer clientsMutex.Unlock()
+	for _, receiver := range clients {
+		if receiver != sender {
+			fmt.Fprint(receiver, message)
+		}
+	}
+}
+
+func removeClient(connection net.Conn) {
+	clientsMutex.Lock()
+	defer clientsMutex.Unlock()
+	for i, client := range clients {
+		if client == connection {
+			clients = append(clients[:i], clients[i+1:]...)
+			return
+		}
 	}
 }
